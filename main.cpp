0b215f85bb0cf09e3f9d91bbf1eac17fa25a2fd4
Antonio Setya <antonio.armyn@gmail.com>
2019-03-30 09:43:07 +0700

    Pentagon with three triangles and gradient color

--- main.cpp
+++ main.cpp
@@ -10,23 +10,39 @@
 void display();
 void init();
 
+// Pentagon vertices
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f, // left  
-    0.5f, -0.5f, 0.0f, // right 
-    0.0f,  0.5f, 0.0f  // top
-};  
+    // positions         // colors
+    0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top
+    -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,// middle left
+    -0.25f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+    0.25f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,// bottom right
+    0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f // middle right
+};
 
-GLuint vbo, vao;
+// Indexes, to tell how to draw the triangles to form a pentagon
+unsigned int indices[] = {
+    0, 1, 2,
+    0, 2, 3,
+    0, 3, 4
+};
+
+// VBO = vertex buffer object | VAO = vertex array object | EBO = element buffer object
+GLuint vbo, vao, ebo;
 GLuint shaderProgram;
 
 const char * vertexSource = R"glsl(
     #version 130
 
     in vec3 position;
+    in vec3 inColor;
+
+    out vec3 vertColor;
 
     void main()
     {
         gl_Position = vec4(position, 1.0);
+        vertColor = inColor;
     }
     
 )glsl";
@@ -34,11 +50,12 @@ const char * vertexSource = R"glsl(
 const char * fragmentSource = R"glsl(
     #version 130
 
+    in vec3 vertColor;
     out vec4 outColor;
 
     void main()
     {
-        outColor = vec4(0.298f, 0.698f, 0.314f, 1.0f);
+        outColor = vec4(vertColor, 1.0f);
     }
 )glsl";
 
@@ -74,9 +91,12 @@ void init()
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
     glCompileShader(vertexShader);
+
+    // Check shader status. If not compiled, terminate 
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
     if (isCompiled == GL_FALSE)
     {   
+        printf("Vertex shader failed to compile...\n");
         GLint maxLength = 0;
         glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
 
@@ -101,9 +121,12 @@ void init()
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
+
+    // Check shader status
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
     if (isCompiled == GL_FALSE)
     {   
+        printf("Fragment shader failed to compile...\n");
         GLint maxLength = 0;
         glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
 
@@ -132,9 +155,11 @@ void init()
     //glBindFragDataLocation(shaderProgram, 0, "outColor");
     glLinkProgram(shaderProgram);
 
+    // Check shader status
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, (int *)&isCompiled);
     if (isCompiled == GL_FALSE)
     {
+        printf("Shader program failed to link...\n");
         GLint maxLength = 0;
         glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
 
@@ -164,13 +189,24 @@ void init()
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     //GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // Positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+    // Colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // Binding position & color array to shader program
+    glBindAttribLocation(shaderProgram, 0, "position");
+    glBindAttribLocation(shaderProgram, 1, "inColor");
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -183,9 +219,10 @@ void display()
 
     glUseProgram(shaderProgram);
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
     
+    /*
     glBegin(GL_POLYGON);
     glColor3ub(76,175,80);
 		glVertex2f(-0.9f, -0.8f);
@@ -194,6 +231,8 @@ void display()
 		glVertex2f(-0.8f, -0.6f);
 		glVertex2f(-0.95f, -0.7f);
     glEnd();
+    */
 
     glFlush();
+    //glFinish();
 }
