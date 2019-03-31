f5062d91ef3ed28163e1a471df762709670dcb42
manasye <manase32@gmail.com>
2019-03-31 08:05:12 +0700

    Finish parsing unicorn image

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,26 +1,43 @@
 import json
 
+
+def getIndexFromLabel(label, points):
+    for point in points:
+        if (point['label'] == label):
+            return point['index']
+    return 0
+
+
 with open('../data/unicorn.json') as f:
     data = json.load(f)
 
 elements = data['geogebra']['construction']['element']
+commands = data['geogebra']['construction']['command']
 
 pointsTxt = []
 points = []
-polygons = []
+polygonsTxt = []
 
+idx = 0
 for element in elements:
     if (element['-type'] == 'point'):
-        points.append({'label': element['-label']})
+        points.append({'index': idx, 'label': element['-label']})
         pointsTxt.append('({},{})'.format(
             float(element['coords']['-x']) / 15, float(element['coords']['-y']) / 15))
-    elif (element['-type'] == 'polygon'):
-        polygons.append(element)
+        idx += 1
 
-print(len(polygons))
+for command in commands:
+    if (command['-name'] == 'Polygon'):
+        i1 = getIndexFromLabel(command['input']['-a0'], points)
+        i2 = getIndexFromLabel(command['input']['-a1'], points)
+        i3 = getIndexFromLabel(command['input']['-a2'], points)
+        polygonsTxt.append('({},{},{})'.format(i1, i2, i3))
+
 
 with open('../data/unicorn.txt', 'w') as f:
     f.write("Points\n")
     for point in pointsTxt:
         f.write("%s\n" % point)
     f.write("Polygon\n")
+    for polygon in polygonsTxt:
+        f.write("%s\n" % polygon)
